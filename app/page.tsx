22a839c5f3c11d7a725f75731d9b32062ef8ae09
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-10-31 09:00:35 +0600

    starting project

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function Page() {
-     return <div>
-          <h1>Here is Page</h1>
+     return <div className="bg-[#F7ECDD] h-screen">
+          <h1 className="flex justify-center items-center text-[12em] font-antiga">IFTEKHAR AHMED</h1>
      </div>
 }
